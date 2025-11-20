19d5e00093f51315a364345a2fbe0db17cb3644d
kimjeasung2 <jsjy2011@naver.com>
2025-11-20 14:46:18 +0900

    실습 파일 업로드(1)

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpStudy
+{
+    internal class Program
+    {
+        //Entry Point 모든 C, C++, C#언어는 Main함수로 시작되고 종료된다.
+        static void Main(string[] args)
+        {
+            //화면 출력하고 다음 줄로 내림
+            //Console.WriteLine(args[0]);
+            //Console.WriteLine(args[1]);
+
+            //화면 출력하고 커서 유지
+            Console.Write("Welcome C# =>");
+            //유저가 작성한 문자입력을 받음. 엔터칠 때까지 
+            Console.WriteLine(Console.ReadLine());
+            //유저가 입력한 처음 키보드 문자를 입력받기
+            ConsoleKeyInfo info = Console.ReadKey();
+            Console.WriteLine(info.KeyChar);
+        }
+    }
+}
